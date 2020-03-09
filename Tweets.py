80497136097dd3e7b2d1cdadb135e6b13433abfc
Lorenzo Marnat <32875283+LorenzoMarnat@users.noreply.github.com>
2020-03-09 17:12:28 +0100

    Wallas et Commit

--- Tweets.py
+++ Tweets.py
@@ -1,25 +1,31 @@
 import tkinter as tk
 import tweepy
+from afinn import Afinn
 
 class CButton:
     def __init__(self,value,button):
         self.value = value
         self.button = button
         
-"récupère les 100 derniers tweets contenant un mot-clé passé en paramètre et les écris dans un fichier texte"
 def getTweetsFromKeyword(keyword):
-    keyword = keyword + " -filter:retweets"
+    k = keyword + " -filter:retweets"
+    afinn = Afinn(language='fr')
     with open("result.txt", "a", encoding="utf-8") as f:
-        search_results = api.search(q=keyword, count=100, geocode="46.27155,2.627197,350km",result_type="recent", lang="fr", tweet_mode ="extended")
+        search_results = api.search(q=k, count=100, geocode="46.27155,2.627197,350km",result_type="recent", lang="fr", tweet_mode ="extended")
         maxid = 0
         c = 0
         for x in range(1,100):
+            print(x)
+            print("x")
             for i in search_results:
                 if(maxid != i.id):
                     c+=1
                     print(c)
+                    print(i.retweet_count)
+                    print(i.favorite_count)
                     print(i.id)
                     print(i.full_text)
+                    print(afinn.score(i.full_text))
                     print('\n')
                     f.write(i.id_str + " ")
                     f.write(i.full_text)
@@ -29,9 +35,9 @@ def getTweetsFromKeyword(keyword):
                     print(i.id)
                     f.close()
                     return
-            search_results  = api.search(q=keyword, count=100, result_type="recent", tweet_mode ="extended", max_id = maxid-1)
-    f.close()
-    
+                search_results  = api.search(q=k, count=100, geocode="46.27155,2.627197,350km",result_type="recent", lang="fr", tweet_mode ="extended", max_id = maxid-1)
+        f.close()
+
 def searchTweets(checkButtons):
      for cb in checkButtons:
          if cb.value.get():
@@ -39,24 +45,24 @@ def searchTweets(checkButtons):
              print(search)
              getTweetsFromKeyword(search)
 
-"récupère les tendances proche d'un endroit passé en paramètre"
 def getTrends(WOEID):
-	trendsP = api.trends_place(WOEID) #Renvoi les tendances sous forme de JSON
-	data = trendsP[0] #Récupère le contenu du JSON
-	trends = data['trends'] #Récupère les éléments 'trends' du JSON
-	names = [trend['name'] for trend in trends] #Récupère l'élément 'name' de chaque tendance
-	return names
+    trendsP = api.trends_place(WOEID) #Renvoi les tendances sous forme de JSON
+    data = trendsP[0] #Récupère le contenu du JSON
+    trends = data['trends'] #Récupère les éléments 'trends' du JSON
+    names = [trend['name'] for trend in trends] #Récupère l'élément 'name' de chaque tendance
+    return names
     
-CONSUMER_KEY = 'v2pSMP8Qq6jpLhk731RdMGSvA'
-CONSUMER_SECRET = 'VLkKvIU2rSh6P5c8suoU8UGLS4NwtEiwOEhqq5W7pbKfe3Bz7P'
-ACCESS_KEY = '1571137290-g61gc41oZtpETZIg2Q4KVP6XVYSNvMzSk57mKm1'
-ACCESS_SECRET = 'iQKVmVk6Vg8SdaXkDhkuAYGHBUB3jpcyXMCXLy9n3rCXa'
+CONSUMER_KEY = 'aQwJP4kxuRRrbOblx4DuXrFGk'
+CONSUMER_SECRET = 'mQilj1fiZ4cDqehXDegrGK8URfvxZgTplH7z4ZJHsYMXYjs50N'
+ACCESS_KEY = '874999178056933376-4OyTKX8gneHJ93yUr6j8XPcUjzAmhZ8'
+ACCESS_SECRET = 'GtOFxjeG4vxGKtR3dk6zDX6bR701jgU9DiTQAET2FhoTi'
 
 PARIS_WOEID = 615702
 
 auth = tweepy.auth.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
 
 top = tk.Tk()
  
